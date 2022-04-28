0193c2fb500df7d8c25dabd7d144341aaa950623
psremnev <52470169+psremnev@users.noreply.github.com>
2022-04-28 22:13:49 +0300

    +

--- app/src/main/java/com/example/home_work_lection_7/AnimateFragment.kt
+++ app/src/main/java/com/example/home_work_lection_7/AnimateFragment.kt
@@ -1,10 +1,12 @@
 package com.example.home_work_lection_7
 
+import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
 import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -12,6 +14,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.core.animation.doOnEnd
 import com.example.home_work_lection_7.databinding.FragmentAnimateBinding
 
 class AnimateFragment : Fragment() {
@@ -30,16 +33,16 @@ class AnimateFragment : Fragment() {
 
         binding.clickMe.setOnClickListener {
             val resources = requireActivity().resources
-            val firstViewColorHolder = PropertyValuesHolder.ofInt("background", resources.getColor(R.color.squareBack), resources.getColor(R.color.squareDestinationBack))
-            val firstViewScaleHolder = PropertyValuesHolder.ofFloat("firstScale", resources.getDimension(R.dimen.squareSize), resources.getDimension(R.dimen.squareDestinationSize))
+            val firstViewColorHolder = PropertyValuesHolder.ofObject("firstBackground", ArgbEvaluator(), (binding.firstView.background as ColorDrawable).color, resources.getColor(R.color.squareDestinationBack))
+            val firstViewScaleHolder = PropertyValuesHolder.ofFloat("firstScale", binding.firstView.layoutParams.height.toFloat(), resources.getDimension(R.dimen.squareDestinationSize))
 
-            val secondViewHeightHolder = PropertyValuesHolder.ofFloat("secondHeight", resources.getDimension(R.dimen.rectangleHeight), resources.getDimension(R.dimen.rectangleDestinationHeight))
+            val secondViewHeightHolder = PropertyValuesHolder.ofFloat("secondHeight", binding.secondView.layoutParams.height.toFloat(), resources.getDimension(R.dimen.rectangleDestinationHeight))
 
             ValueAnimator.ofPropertyValuesHolder(firstViewColorHolder, firstViewScaleHolder, secondViewHeightHolder).apply {
                 duration = 400L
                 interpolator = AccelerateDecelerateInterpolator()
                 addUpdateListener {
-                    val firstColor = getAnimatedValue("background") as Int
+                    val firstColor = getAnimatedValue("firstBackground") as Int
                     val firstScale = getAnimatedValue("firstScale") as Float
                     binding.firstView.setBackgroundColor(firstColor)
                     val firstLayoutParams = binding.firstView.layoutParams
@@ -53,6 +56,7 @@ class AnimateFragment : Fragment() {
                     secondLayoutParams.height = secondHeight.toInt()
                     binding.secondView.layoutParams = secondLayoutParams
                 }
+                this.setupStartValues()
                 start()
             }
         }
